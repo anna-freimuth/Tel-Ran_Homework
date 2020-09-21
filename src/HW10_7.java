58a44e5fe3e72e88b590c4f6d41839c19411a742
Anna Freimuth <a.freimuth90@gmail.com>
2020-09-21 19:41:47 +0200

    Changed the code according to the teacher suggestions.

--- src/HW10_7.java
+++ src/HW10_7.java
@@ -1,17 +1,14 @@
 public class HW10_7 {
     public static void main(String[] args) {
-        yearLeap(2020);
-        yearLeap(2021);
-        yearLeap(2000);
+        System.out.println(yearLeap(2020));
+        System.out.println(yearLeap(2021));
+        System.out.println(yearLeap(2000));
     }
 
-    private static void yearLeap(int year) { //Task 7 Leap year or no
-        if (year % 4 == 0 && year % 100 != 0) {
-            System.out.println("This year is a leap year: " + year);
-        } else if (year % 400 == 0) {
-            System.out.println("This year is a leap year: " + year);
-        }else {
-            System.out.println("This year is not a leap year: " + year);
-        }
+    private static boolean yearLeap(int year) { //Task 7 Leap year or no
+        if (year % 4 == 0 && year % 100 != 0) return true;
+        else if (year % 400 == 0)  return true;
+        else return false;
+
     }
 }
