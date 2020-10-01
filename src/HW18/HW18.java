6ec7aea8200a65f29ca6114e3c820c4ffe223431
Anna Freimuth <a.freimuth90@gmail.com>
2020-10-01 21:21:25 +0200

    WIP: Full tiles amount is no longer area based.

--- src/HW18/HW18.java
+++ src/HW18/HW18.java
@@ -17,17 +17,17 @@ public class HW18 {
         // Учтите, что плитку можно класть как вдоль, так и поперёк и только на месте мастер узнает,
         // как именно её надо класть.
         // Стоимость материала
-       // Стоимость рабочего времени
-       //  Итоговую ориентировочную стоимость
+        // Стоимость рабочего времени
+        //  Итоговую ориентировочную стоимость
 
         //1.find amount of tile vertically depends on floor/wall (full and cut) meters to cm
         //2.find amount of tile horizontally depends on floor/wall (full and cut) meters to cm
         //3.find cost of materials (horizontally and vertically) , cost per tile my
         //4.find cost of hors work(27e per hur)
         //5.Final cost (with vat or without?)
-        long fullTiles = findAmountOfFullTiles(length,width);
-        double costFullAmountOfHours = findServiceCostForFullAmountOfHours(length,width);
-        double costForMaterials = findCostOfMaterials(singleTileCost,fullTiles);
+        long fullTiles = findAmountOfFullTiles(length * 100, width * 100);
+        double costFullAmountOfHours = findServiceCostForFullAmountOfHours(length, width);
+        double costForMaterials = findCostOfMaterials(singleTileCost, fullTiles);
         double finalCost = costFullAmountOfHours + costForMaterials;
 
         System.out.println("Amount of tiles: " + fullTiles);
@@ -36,18 +36,20 @@ public class HW18 {
         System.out.println("Total: " + finalCost);
     }
 
-    private static double findServiceCostForFullAmountOfHours(long lengthOfWallOrFloor,long widthOfWallOrFloor) {
+    private static double findServiceCostForFullAmountOfHours(long length, long width) {
         long costPerHour = 27;
-        return costPerHour * Math.ceil(lengthOfWallOrFloor * widthOfWallOrFloor);
-
+        return costPerHour * Math.ceil(length * width);
     }
 
-    private static double findCostOfMaterials(double pricePerTile, long fullTiles ) {
-     return  pricePerTile * fullTiles;
+    private static double findCostOfMaterials(double pricePerTile, long fullTiles) {
+        return pricePerTile * fullTiles;
     }
 
-    private static long findAmountOfFullTiles(long lengthOfWallOrFloor,long widthOfWallOrFloor) { //in cm
-        long OneHorizontalTile = 30 * 20;
-        return lengthOfWallOrFloor * widthOfWallOrFloor * 10000 / OneHorizontalTile;
+    private static long findAmountOfFullTiles(long length, long width) {
+        long tileLength = 30;
+        long tileWidth = 20;
+        long lengthResult = length / tileLength;
+        long widthResult = width / tileWidth;
+        return lengthResult * widthResult;
     }
 }
