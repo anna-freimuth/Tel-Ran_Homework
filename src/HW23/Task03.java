ecd930d0670425a7390e85b7a3862727f8a9fba6
Anna Freimuth <a.freimuth90@gmail.com>
2020-10-09 22:31:19 +0200

    Added second version without regex.

--- src/HW23/Task03.java
+++ src/HW23/Task03.java
@@ -5,9 +5,23 @@ public class Task03 {
         System.out.println(zipZap("zipXzap")); // → "zpXzp"
         System.out.println(zipZap("zopzop")); // → "zpzp"
         System.out.println(zipZap("zzzopzop")); // → "zzzpzp"
+        System.out.println(zipZap2("zipXzap")); // → "zpXzp"
+        System.out.println(zipZap2("zopzop")); // → "zpzp"
+        System.out.println(zipZap2("zzzopzop")); // → "zzzpzp"
     }
 
     private static String zipZap(String str) {
         return str.replaceAll("z.p", "zp");
+    }
+
+    private static String zipZap2(String str) {
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'z' && str.length() - 2 > i && str.charAt(i + 2) == 'p') {
+
+                str = str.substring(0, i + 1) + str.substring(i + 2);
+            }
+        }
+        return str;
     }
 }
