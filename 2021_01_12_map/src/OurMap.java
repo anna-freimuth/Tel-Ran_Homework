5d95cc5e744eacdb15109ddebb3266151f83aac3
Anna Freimuth <a.freimuth90@gmail.com>
2021-01-12 15:29:30 +0100

    map

---
+++ 2021_01_12_map/src/OurMap.java
@@ -0,0 +1,15 @@
+import java.util.Iterator;
+
+public interface OurMap<K, V> {
+    public V put(K key, V value);
+
+    public V get(K key);
+
+    public V remove(K kex);
+
+    public int size();
+
+    public Iterator<K> keyIterator();
+
+    public Iterator<V> valueIterator();
+}
