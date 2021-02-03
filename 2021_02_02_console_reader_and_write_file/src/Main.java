b33f08ca8fdb7a5afc2e5032881689c47137c096
Anna Freimuth <a.freimuth90@gmail.com>
2021-02-03 10:50:47 +0100

    update

--- 2021_02_02_console_reader_and_write_file/src/Main.java
+++ 2021_02_02_console_reader_and_write_file/src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        BufferReader bufferReader = new BufferReader("2021_02_02_console_reader/src/output.txt");
+        BufferReader bufferReader = new BufferReader("2021_02_02_console_reader_and_write_file/src/output.txt");
         bufferReader.readFromConsoleAndWriteToFile();
     }
 }
