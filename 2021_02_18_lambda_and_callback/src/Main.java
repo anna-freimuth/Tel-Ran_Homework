b5c62fcfe49b4b1ea3a1536a9cfb7f0ccfaf5aee
Anna Freimuth <a.freimuth90@gmail.com>
2021-02-18 17:00:46 +0100

    lambda, callback

---
+++ 2021_02_18_lambda_and_callback/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> listWithDuplicates = Arrays.asList("movie", "fitness", "fitness", "restaurant", "bowling", "bowling");
+        Function<List<String>, List<String>> task1 = (listWithoutDuplicates) -> listWithDuplicates.stream().distinct().collect(Collectors.toList());
+        System.out.println(task1.apply(listWithDuplicates));
+
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        Function<Integer, Integer> task2 = (x) -> a * x * x + b * x + c;
+        System.out.println(task2.apply(5));
+    }
+}
---
+++ 2021_02_18_lambda_and_callback/src/task3.md
@@ -0,0 +1,7 @@
+|||
+|---|---|
+|IntSupplier|                                 `() -> 3;`|
+|Consumer<String>|                          `System.out :: println;`|
+|BiPredicate<Integer,Integer>|               `(x, y)-> x % y == 0;`|
+|DoubleUnaryOperator |                        `Math :: sin;`   |                                     
+|Function<Double,String> |                   `(x)->String.valueOf(x * x);`|
