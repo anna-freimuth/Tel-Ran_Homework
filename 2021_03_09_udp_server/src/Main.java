db8476a4c752793ea9c3528e96a0aeed426d217b
Anna Freimuth <a.freimuth90@gmail.com>
2021-03-09 14:46:06 +0100

    udp

---
+++ 2021_03_09_udp_server/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Main {
+
+    private final static int UDP_SERVER_PORT = 3000;
+    private static final int PACKET_SIZE = 1024;
+
+    public static void main(String[] args) throws IOException {
+
+        DatagramSocket serverUdpSocket = new DatagramSocket(UDP_SERVER_PORT);
+        byte[]dataIn = new byte[PACKET_SIZE];
+        DatagramPacket packetIn = new DatagramPacket(dataIn,PACKET_SIZE);
+
+        while(true){
+            serverUdpSocket.receive(packetIn);
+            String line = new String(dataIn,0, packetIn.getLength());
+
+            String response = "Handled a datagram from the port:  " +  packetIn.getPort() + ", received data  "+ line;
+
+            byte[] dataOut = response.getBytes();
+            DatagramPacket packetOut = new DatagramPacket(  // packet that we are creating for response to client
+                    dataOut,
+                    dataOut.length,
+                    packetIn.getAddress(),
+                    packetIn.getPort()
+            );
+
+            serverUdpSocket.send(packetOut);
+        }
+    }
+}
